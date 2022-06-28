5f2f62a41564a03ba1a68f7a3882f553b5d761f9
Nicole MacLean <nicole.mac0404@gmail.com>
2022-06-28 12:08:00 -0700

    Daily leet - had to use solution

---
+++ addTwoNumbersLinkedList.js
@@ -0,0 +1,36 @@
+const ListNode = (val, next) => {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+};
+
+const addTwoNumbers = function (l1, l2) {
+  let List = new ListNode(0);
+  let head = List;
+  let sum = 0;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null || sum > 0) {
+    if (l1 !== null) {
+      sum = sum + l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum = sum + l2.val;
+      l2 = l2.next;
+    }
+    if (sum >= 10) {
+      carry = 1;
+      sum = sum - 10;
+    }
+
+    head.next = new ListNode(sum);
+    head = head.next;
+
+    sum = carry;
+    carry = 0;
+  }
+
+  return List.next;
+};
+
+// Needed to use solution, could not figure this one out alone. Understand, but couldn't get it alone.
