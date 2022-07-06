04622e2407b4884f3e567ece65d98f20fb97213f
Nicole MacLean <nicole.mac0404@gmail.com>
2022-07-06 14:19:07 -0700

    Easy leet

---
+++ climbStairsFibonacci.js
@@ -0,0 +1,18 @@
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+
+const climbStairs = function (n) {
+  let a = 1;
+  let b = 2;
+  let next;
+
+  for (let i = 3; i <= n; i++) {
+    next = a + b;
+    a = b;
+    b = next;
+  }
+  return n === 1 ? a : b;
+};
