0969003f0a6ffecf4986018a705c16d807c75f59
Nicole MacLean <nicole.mac0404@gmail.com>
2022-07-07 11:08:07 -0700

    Fail, order of check

--- validPalindrome2.js
+++ validPalindrome2.js
@@ -1,28 +1,48 @@
 const validPalindrome = function (s) {
+  const isPalindrome = function (s) {
+    let point1 = 0;
+    let point2 = s.length - 1;
+    while (point1 < point2) {
+      if (s[point1] === s[point2]) {
+        point1 += 1;
+        point2 -= 1;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  };
+
   const lower = s.toLowerCase().replace(/[^0-9a-z]/gi, "");
   let array = [...lower];
 
   let characters = 0;
   while (array.length >= 1) {
-    console.log(array, characters);
     if (characters >= 2) {
-      console.log(characters, "return in chara");
       return false;
     }
-    let point2 = array.length - 1;
-    if (array[0] === array[point2]) {
+    let end = array.length - 1;
+    if (array[0] === array[end]) {
       array.pop();
       array.shift();
-    } else if (array[1] === array[point2]) {
-      array.pop();
-      array.shift();
-      array.shift();
-      characters += 1;
-    } else if (array[0] === array[point2 - 1]) {
-      array.pop();
-      array.pop();
-      array.shift();
-      characters += 1;
+    } else if (array[1] === array[end] || array[0] === array[end - 1]) {
+      if (
+        array[1] === array[end] &&
+        isPalindrome(array.slice(array[2], array[end - 1]))
+      ) {
+        array.pop();
+        array.shift();
+        array.shift();
+        characters += 1;
+      } else if (
+        array[0] === array[end - 1] &&
+        isPalindrome(array.slice(array[1], array[end - 2]))
+      ) {
+        array.pop();
+        array.pop();
+        array.shift();
+        characters += 1;
+      }
     } else {
       return false;
     }
