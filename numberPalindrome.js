11e3bc79f11d16ddb12639cc0b743ac576aee096
Nicole MacLean <nicole.mac0404@gmail.com>
2022-07-21 16:36:27 -0700

    Both attempts num pal

--- numberPalindrome.js
+++ numberPalindrome.js
@@ -1,4 +1,42 @@
+// Given an integer x, return true if x is palindrome integer.
+
+// An integer is a palindrome when it reads the same backward as forward.
+
+// For example, 121 is a palindrome while 123 is not.
+
+// First attempt June 6 2022
 const isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+  if (x < 10) {
+    return true;
+  }
+
+  const numStr = x.toString();
+
+  let loopTimes = 0;
+
+  if (numStr.length % 2 === 0) {
+    loopTimes = numStr.length / 2;
+  } else {
+    loopTimes = Math.floor(numStr.length / 2);
+  }
+
+  let descIndex = numStr.length - 1;
+
+  for (let i = 0; i < loopTimes; i++) {
+    if (numStr[i] !== numStr[descIndex]) {
+      return false;
+    }
+    descIndex -= 1;
+  }
+
+  return true;
+};
+
+// Second attempt July 21 2022
+const isPalindromeSecond = function (x) {
   if (x < 0) {
     return false;
   }
