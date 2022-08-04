b60b7364f79178d98411d8eb3d7a9be072061590
Nicole MacLean <nicole.mac0404@gmail.com>
2022-08-03 19:26:16 -0700

    Commas fixed

--- zigzag.js
+++ zigzag.js
@@ -4,6 +4,9 @@
  * @return {string}
  */
 const convert = function (s, numRows) {
+  if (numRows > s.length) {
+    return s;
+  }
   // Create array of arrays to sort letters into
   let arraySort = [];
   for (let j = 0; j < numRows; j++) {
@@ -11,22 +14,29 @@ const convert = function (s, numRows) {
   }
   // String to array for easy iteration
   let stringToArray = s.split("");
+  // console.log(stringToArray)
 
   while (stringToArray.length > 0) {
     for (let i = 0; i < numRows; i++) {
       let letter = stringToArray.shift();
+
       if (letter) {
+        console.log(letter);
         arraySort[i].push(letter);
       }
     }
     for (let k = numRows - 2; k >= 1; k--) {
       let letter = stringToArray.shift();
+
       if (letter) {
+        console.log(letter);
         arraySort[k].push(letter);
       }
     }
   }
+
   let final = arraySort.toString();
+
   let finalCommaFree = "";
   for (let f = 0; f < final.length; f++) {
     if (f % 2 === 0) {
