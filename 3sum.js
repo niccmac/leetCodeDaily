2c67f808d2074547799ce5a04e8965a01b580316
Nicole MacLean <nicole.mac0404@gmail.com>
2022-08-23 10:29:33 -0700

    Correct

--- 3sum.js
+++ 3sum.js
@@ -1,39 +1,30 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-const threeSum = (nums) => {
-  const results = [];
-  if (nums.length < 3) return results;
-  nums = nums.sort((a, b) => a - b);
-  let target = 0;
+const threeSum = function (nums) {
+  let result = [];
+  const target = 0;
+  if (nums.length < 3) return result;
+  nums.sort((a, b) => a - b);
 
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (nums[i] > target) break;
-    if (i > 0 && nums[i] === nums[i - 1]) {
-      let j = i + 1;
+  for (let i = 0; i < nums.length; i++) {
+    let low = i + 1;
+    let high = nums.length - 1;
+    let sum = 0;
 
-      let k = nums.length - 1;
+    while (low < high) {
+      sum = nums[i] + nums[low] + nums[high];
 
-      while (j < k) {
-        let sum = nums[i] + nums[j] + nums[k];
-
-        if (sum === target) {
-          results.push([nums[i], nums[j], nums[k]]);
-
-          while (nums[j] === nums[j + 1]) j++;
-          while (nums[k] === nums[k - 1]) k--;
-
-          j++;
-          k--;
-        } else if (sum < target) {
-          j++;
-        } else {
-          k--;
-        }
+      if (sum === target) {
+        result.push([nums[i], nums[low], nums[high]]);
+        while (nums[low + 1] === nums[low]) low++;
+        while (nums[high - 1] === nums[high]) high--;
+        low++;
+        high--;
+      } else if (sum < target) {
+        low++;
+      } else {
+        high--;
       }
     }
+    while (nums[i + 1] === nums[i]) i++;
   }
-
-  return results;
+  return result;
 };
